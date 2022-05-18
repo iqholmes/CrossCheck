1fc0a2ffb7b4cc9c3ff1b20ced9d349712b130ba
Ian Holmes <iqholmes@gmail.com>
2022-05-18 14:22:46 -0400

    athlete field populates based on user when posting score

--- client/src/components/post/post-score-view.js
+++ client/src/components/post/post-score-view.js
@@ -12,14 +12,14 @@ const PostScoreView = () => {
   const [show, setShow] = useState(false);
   const [reps, setReps] = useState(Number);
   const [time, setTime] = useState('');
-  const [athlete, setAthlete] = useState('');
-  const email = useSelector((state) => state.auth.email);
   const firstName = useSelector((state) => state.auth.firstName);
   const lastName = useSelector((state) => state.auth.lastName);
   const authenticated = useSelector((state) => state.auth.authenticated);
+  const userString = `${firstName} ${lastName}`
+  const [athlete, setAthlete] = useState(userString);
+  const email = useSelector((state) => state.auth.email);
 
   const { workout } = useSelector((state) => state.workoutData);
-  console.log(firstName, lastName, email);
   const dispatch = useDispatch();
   
   useEffect(() => {
@@ -117,7 +117,7 @@ const PostScoreView = () => {
                     className="form-control"
                     placeholder="Enter Your Name"
                     value={athlete}
-                    onChange={(e) => setAthlete(e.target.value)} 
+                    onChange={(e) => setAthlete(userString)} 
                   ></input>
                   <label>Reps</label>
                   <input
